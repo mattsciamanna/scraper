d740ac8887bf8f36a467dc0756015050966cf73b
mattsciamanna <35858537+mattsciamanna@users.noreply.github.com>
2019-02-26 15:45:15 -0500

    Update awsscrape.py

--- awsscrape.py
+++ awsscrape.py
@@ -47,7 +47,7 @@ errorfile = os.path.join(fileDir, 'realscrapeout/errors' + sys.argv[1] + str(tod
 # Replaces with your configuration information
 redis_host = "redis-17827.c57.us-east-1-4.ec2.cloud.redislabs.com"
 redis_port = 17827
-redis_password = "dFFYSFXQlu5E28IrN8fL6Y7yXt7iX4Hm"
+redis_password = "PASSWORD"
 
 #got rid of try except because if this fails, we're gonna wanna know.
 
